d9cdc5181b38dd05dc2d2bae05f2661f5f31f162
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-01-05 23:51:23 +0530

    create the saveEmployee function in js file

--- js/employee.js
+++ js/employee.js
@@ -1,4 +1,29 @@
 function saveEmployee(){
+    let name=$('#exampleFormControlInput2').val(); //get value from input field
+    let address=$('#exampleFormControlInput3').val();
+    let number=$('#exampleFormControlInput4').val();
+    //console.log(name);
 
-    let
+
+    //send the http request in to backend using ejections
+    $.ajax( {
+        method:"POST",
+        contentType:"application/json" , //content tye we communicate with json type
+        url:"http://localhost:8080/api/v1/employee/saveEmployee", //this is the url of when pass the data in backend
+        async:true,
+        data:JSON.stringify( {   //when pass the values from to the backend getting frontend
+            "empID":" ",
+            "empName":name,
+            "empAddress":address,
+            "empMNumber":number
+        }),
+        success:function(data){
+        alert("Successfully saved employee")
+        },
+        error:function(xhr,exception){
+            alert("Error")
+        }
+
+
+    })
 }
